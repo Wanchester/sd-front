d39c7a6f5bd0a192a0a756947e4cb005ed4032f6
duynguyen02704@gmail.com <duynguyen02704@gmail.com>
2022-05-16 13:30:37 +1000

    add avatar component with the resize event

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, { Fragment } from "react";
 import { GlobalStyle } from "./GlobalStyle";
 import HelloWorld from "./HelloWorld";
+import HomePage from "./HomePage"
 const App = () => {
   return (
     <Fragment>
+      <HomePage />
       <HelloWorld />
       <GlobalStyle />
     </Fragment>
---
+++ src/HomePage.tsx
@@ -0,0 +1,10 @@
+import react from 'react'
+import Avatar from './components/avatar/Avatar'
+import TestImage from './image/test.jpeg'
+const HomePage = () => {
+    return(
+        <Avatar imageLink = {TestImage}/>
+    )
+}
+
+export default HomePage
