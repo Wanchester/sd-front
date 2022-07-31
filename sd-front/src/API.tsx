1e7a8a0200c0428e1a1525c819f5a14ca92d8509
duynguyen02704@gmail.com <duynguyen02704@gmail.com>
2022-07-31 11:03:50 +1000

    first commit

---
+++ sd-front/src/API.tsx
@@ -0,0 +1,18 @@
+import axios from "axios";
+const BASE_URL = "http://54.210.74.109/api/";
+
+const defaultConfig = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+const apiMethods = {
+  getPlayer: async (userID: { userID: string }) => {
+    const endpoint = `${BASE_URL}/profile/${userID}`;
+    return (await fetch(endpoint)).json();
+  },
+};
+
+export default apiMethods;
