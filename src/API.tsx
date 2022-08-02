11a8ee73661a8e421ab19c0ba0617114a3012c56
duynguyen02704@gmail.com <duynguyen02704@gmail.com>
2022-08-02 16:56:36 +1000

    solve the merge conflict

--- src/API.tsx
+++ src/API.tsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-<<<<<<< HEAD
 export interface ProfileResponse {
   username: string;
   name: string;
@@ -24,26 +23,16 @@ const apiMethods = {
   getPlayer: async (userID: string) => {
     return axios
       .get(`/api/profile/${userID}`, {
-=======
-const apiMethods = {
-  getPlayer: async (userID: string) => {
-    return axios
-      .get("/api/profile/p_jbk", {
->>>>>>> f96e761a243e9d5550684b9df86dd975361bb23d
         headers: {
           "Content-Type": "application/json",
         },
       })
-<<<<<<< HEAD
       .then((response) => response.data as ProfileResponse);
   },
   postPlayer: async (player: ProfileResponse) => {
     return axios
       .post(`/api/profile/${player.username}`, JSON.stringify(player))
       .then((response) => console.log(response))
-=======
-      .then((response) => console.log(response.data))
->>>>>>> f96e761a243e9d5550684b9df86dd975361bb23d
       .catch((err) => console.log(err));
   },
 };
