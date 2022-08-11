25814526c60ea2fa659f16fa44e3443725782cef
Santiago <andresnavarro3@hotmail.com>
2022-08-12 00:22:36 +1000

    Norse

---
+++ sd-front/src/components/Nav/Sidebar.Styles.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const NavS = styled.p`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  height: 5rem;
+  background-color: black;
+`;
+export const Sidebar = styled.p<{ sidebar: boolean }>`
+  width: 250px;
+  height: 100vh;
+  background-color: black;
+  position: fixed;
+  top: 10;
+  left: ${({ sidebar }) => (sidebar ? "0" : "-100%")}
+`;
+export const NavsIcon = styled.p`
+  font-size: 2rem;
+  margin-left: 3rem;
+`;
+export const SideWrap = styled.p`
+  font-size: 2rem;
+  margin-left: 2rem;
+`;
+
+export const NavsIcon2 = styled.p`
+  font-size: 2rem;
+  margin-left: 1rem;
+  margin-top: 4rem;
+`;
