e32a68e9d1c23fb43be6ac6d382bf92f97e061fe
duynguyen02704@gmail.com <duynguyen02704@gmail.com>
2022-08-24 14:38:20 +1000

    add loading while querying

--- src/TeamPage.tsx
+++ src/TeamPage.tsx
@@ -12,26 +12,7 @@ const SliderProps = {
   pageTransition: 500, // Transition time when flipping pages
 };
 const TeamPage: React.FC = () => {
-  const [playerList, setPlayerList] = useState({
-    players: [
-      {
-        name: "Exon",
-        username: "p_exon",
-      },
-      {
-        name: "Nolan",
-        username: "p_nolan",
-      },
-      {
-        name: "T Mac",
-        username: "p_tmac",
-      },
-      {
-        name: "Warren",
-        username: "p_warren",
-      },
-    ],
-  } as PlayerList);
+  const [playerList, setPlayerList] = useState(null as PlayerList | null);
   // const [data, setData] = useState<PlayerList[]>([]);
   // const [isDialogOpen, setIsDialogOpen] = useState(false);
   // const [activePlayer, setActivePlayer] = useState<PlayerList>();
@@ -50,31 +31,37 @@ const TeamPage: React.FC = () => {
   if (playerList && playerList.players.length < 1) return <div>Loading </div>;
 
   return (
-    <Container fluid p-0>
-      <Row>
-        <Col md={{ span: 7, offset: 2 }}>
-          <h2>Player List</h2>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Slider {...SliderProps}>
-            {playerList &&
-              playerList.players.map((p) => (
-                <Link to={`/player/${p.username}`}>
-                  <div key={p.name}>
-                    <img
-                      src="https://st.depositphotos.com/2101611/3925/v/600/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg"
-                      alt="xd"
-                    />
-                    <>{p.name}</>
-                  </div>
-                </Link>
-              ))}
-          </Slider>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      {playerList ? (
+        <Container fluid p-0>
+          <Row>
+            <Col md={{ span: 7, offset: 2 }}>
+              <h2>Player List</h2>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Slider {...SliderProps}>
+                {playerList &&
+                  playerList.players.map((p) => (
+                    <Link to={`/player/${p.username}`}>
+                      <div key={p.name}>
+                        <img
+                          src="https://st.depositphotos.com/2101611/3925/v/600/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg"
+                          alt="xd"
+                        />
+                        <>{p.name}</>
+                      </div>
+                    </Link>
+                  ))}
+              </Slider>
+            </Col>
+          </Row>
+        </Container>
+      ) : (
+        <>Loading...</>
+      )}
+    </>
   );
 };
 export default TeamPage;
